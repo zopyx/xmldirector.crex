4682efe28c8925e04d7c4afb1bd42b31c4eedd51
zopyx <yet@gmx.de>
2015-12-03 15:11:07 +0100

    upload form

--- xmldirector/crex/browser/demo.py
+++ xmldirector/crex/browser/demo.py
@@ -5,6 +5,7 @@
 
 
 import os
+import tempfile
 from xmldirector.crex.browser import api
 
 from Products.Five.browser import BrowserView
@@ -12,7 +13,14 @@ from Products.Five.browser import BrowserView
 
 class Demo(BrowserView):
 
-    def demo(self):
-        zip_file = os.path.join(os.path.dirname(__file__), 'data', 'sample.zip')
-        result = api.convert_crex(zip_file)
-        return result
+    def convert(self):
+
+        zip_tmp = tempfile.mktemp(suffix='.zip')
+        with open(zip_tmp, 'wb') as fp:
+            fp.write(self.request.form['zipfile'].read())
+        zip_out = api.convert_crex(zip_tmp)
+        with open(zip_out, 'rb') as fp:
+            self.request.response.setHeader('content-type', 'application/zip')
+            self.request.response.setHeader('content-disposition', 'attachment;filename={}'.format('c-rex-converted.zip'))
+            self.request.response.setHeader('content-length', str(os.path.getsize(zip_out)))
+            self.request.response.write(fp.read())
