4061de0a47ddec134ed89de2a7c009754bec3a8e
zopyx <yet@gmx.de>
2015-12-03 13:48:29 +0100

    updated

---
+++ xmldirector/crex/i18n.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+################################################################
+# onkopedia.crex
+# (C) 2015,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
+################################################################
+
+try:
+    from zope.i18nmessageid import MessageFactory
+    MessageFactory = MessageFactory('xmldirector.crex')
+except ImportError:
+    pass
---
+++ xmldirector/crex/interfaces.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+################################################################
+# onkopedia.crex
+# (C) 2015,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
+################################################################
+
+from zope.interface import Interface
+from zope import schema
+from xmldirector.crex.i18n import MessageFactory as _
+
+
+class IBrowserLayer(Interface):
+    """A brower layer specific to my product """
+
+
+class ICRexSettings(Interface):
+    """ C-Rex settings """
+
+    crex_conversion_url = schema.TextLine(
+        title=_(u'URL for C-REX conversion webservice'),
+        description=_(u'URL for C-REX conversion webservice'),
+        default=u'https://c-rex.net'
+    )
+
+    crex_conversion_username = schema.TextLine(
+        title=_(u'Username for C-REX conversion webservice'),
+        description=_(u'Username for C-REX conversion webservice'),
+        default=u'onkopedia@dgho.de'
+    )
+
+    crex_conversion_password = schema.TextLine(
+        title=_(u'Password for C-REX conversion webservice'),
+        description=_(u'Password for C-REX conversion webservice'),
+        default=u'Eeh[}gX;'
+    )
+
+    crex_conversion_token = schema.TextLine(
+        title=_(u'C-REX Conversion Token'),
+        description=_(u'C-REX Conversion Token'),
+        default=u''
+    )
+
+
