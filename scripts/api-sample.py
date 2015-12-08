9c1f00b4b744557f29a8018b92dbde241d1e7cac
zopyx <yet@gmx.de>
2015-12-08 12:24:59 +0100

    updated

--- scripts/api-sample.py
+++ scripts/api-sample.py
@@ -18,6 +18,14 @@ payload = dict(
         hello=u'world')
 )
 
+print '-'*80
+print 'SEARCH'
+result = requests.get(url + '/@@API/xmldirector/search', auth=HTTPBasicAuth(user, password), headers=headers,data=json.dumps(payload))
+data = result.json()
+pprint.pprint(data)
+
+print '-'*80
+print 'CREATE'
 result = requests.post(url + '/@@API/xmldirector/create', auth=HTTPBasicAuth(user, password), headers=headers,data=json.dumps(payload))
 data = result.json()
 id = data['id']
@@ -25,11 +33,15 @@ url = data['url']
 print(id)
 print (url)
 
+print '-'*80
+print 'GET_METADATA'
 result = requests.get(url + '/@@API/xmldirector/get_metadata/', auth=HTTPBasicAuth(user, password), data=json.dumps(payload))
 data = result.json()
 pprint.pprint(data)
 
 
+print '-'*80
+print 'SET_METADATA'
 payload = dict(
     title=u'new title: hello world',
     description=u'New description'
@@ -38,17 +50,23 @@ result = requests.post(url + '/@@API/xmldirector/set_metadata', auth=HTTPBasicAu
 print result
 
 
+print '-'*80
+print 'GET_METADATA'
 result = requests.get(url + '/@@API/xmldirector/get_metadata/', auth=HTTPBasicAuth(user, password), data=json.dumps(payload))
 data = result.json()
 pprint.pprint(data)
 
 
+print '-'*80
+print 'CONVERT'
 files = {'file': open('sample.zip', 'rb')}
 result = requests.post(url + '/@@API/xmldirector/convert/', auth=HTTPBasicAuth(user, password), files=files)
 data = result.json()
 pprint.pprint(data)
 
 
+print '-'*80
+print 'DELETE'
 result = requests.get(url + '/@@API/xmldirector/delete/', auth=HTTPBasicAuth(user, password), headers=headers,data=json.dumps(payload))
 data = result.json()
 pprint.pprint(data)
